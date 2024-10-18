c9e322d0299ba5c5aa26c2822dbf7b40d4270fa1
gaierSiemens <jakub.gaier@siemens.com>
2024-10-18 09:08:13 +0200

    Update license format (#16)

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # QR Code Scanner App, which sends scanned QR Code to PLC
-# Copyright 2023 Siemens AG
+# Copyright (c) Siemens 2023
 # This file is subject to the terms and conditions of the MIT License.  
 # See LICENSE file in the top-level directory.
 # Date: 22.12.2023
